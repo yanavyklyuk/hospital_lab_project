b96c641f0e9c2791b4afcbc016d0142b79726200
yanavyklyuk <yana.vykliuk.shi.2023@lpnu.ua>
2024-10-23 01:36:42 +0300

    Changed settings

--- hospital/hospital/settings.py
+++ hospital/hospital/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from .db_connection import DATABASES
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -72,18 +73,7 @@ WSGI_APPLICATION = "hospital.wsgi.application"
 
 
 # Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        'ENGINE': '',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
-    }
-}
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databasese
 
 
 # Password validation
