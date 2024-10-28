4b99956fbc40ea1a81ac5ca5f9e3f45b7b43e434
yanavyklyuk <yana.vykliuk.shi.2023@lpnu.ua>
2024-10-28 16:55:11 +0200

    Add some modifications

--- hospital/hospital_api/serializers/doctor_serializer.py
+++ hospital/hospital_api/serializers/doctor_serializer.py
@@ -1,11 +1,15 @@
 from rest_framework import serializers
+
 from repository.models.doctor import Doctor
-from .specialisation_serializer import SpecialisationSerializer
+from repository.repositories.repository_manager import RepositoryManager
 
 
 class DoctorSerializer(serializers.ModelSerializer):
-    specialisation = SpecialisationSerializer
+    specialisation = serializers.SlugRelatedField(read_only=True, slug_field='name')
+    specialisation_id = serializers.PrimaryKeyRelatedField(queryset=RepositoryManager().specialisations.get_all(),
+                                                           source='specialisation', write_only=True, required=True)
 
     class Meta:
         model = Doctor
-        fields = ['id', 'first_name', 'last_name', 'specialisation', 'practice_start_date']
+        fields = ['first_name', 'last_name', 'sex', 'date_birth', 'phone_number', 'practice_start_date',
+                  'specialisation','education']
--- hospital/hospital_api/serializers/schedule_serializer.py
+++ hospital/hospital_api/serializers/schedule_serializer.py
@@ -1,11 +1,15 @@
 from rest_framework import serializers
+
 from repository.models.schedule import Schedule
 from doctor_serializer import DoctorSerializer
+from repository.repositories.repository_manager import RepositoryManager
 
 
 class ScheduleSerializer(serializers.ModelSerializer):
-    doctor = DoctorSerializer
+    doctor = DoctorSerializer(read_only=True)
+    doctor_id = serializers.PrimaryKeyRelatedField(queryset=RepositoryManager().doctors.get_all(), source='doctor',
+                                                   write_only=True, required=True)
 
     class Meta:
         model = Schedule
-        fields = ['id', 'day', 'doctor', 'start_time', 'end_time', 'cabinet_number']
+        fields = ['day', 'doctor', 'start_time', 'end_time', 'minutes_per_patient', 'cabinet_number']
