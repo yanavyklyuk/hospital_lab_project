6b272bb9a077b4eef16af5d59ee75fe6a19f4989
Ihor Yatsyshyn <ihoryatsyshyn4@gmail.com>
2024-10-28 14:31:31 +0200

    Created serializers for some models (#6)
    * Created serializers for some models
    
    * Revert "Created serializers for some models"
    
    This reverts commit 21a9828576bd7aec1428f870a3fd022c956ef90e.
    
    * Created serializers for some models

---
+++ hospital/hospital_api/serializers/favor_serializer.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from repository.models.favor import Favor
+
+
+class FavorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Favor
+        fields = ['id', 'name', 'cost']
