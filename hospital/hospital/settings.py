b90349f161990ee4b4d66ea1e7ce02ff15fc55f7
yanavyklyuk <yana.vykliuk.shi.2023@lpnu.ua>
2024-11-18 18:54:12 +0200

    Changed settings

--- hospital/hospital/settings.py
+++ hospital/hospital/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     "hospital_api",
     "frontend",
     "dashboard",
-    #"channels",
+    "channels",
 ]
 
 MIDDLEWARE = [
@@ -129,3 +129,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
